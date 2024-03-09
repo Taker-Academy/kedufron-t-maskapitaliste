6515238055eb92f167bf9cebabd437d8d3efc1fb
Ulysse Aubree <ulysse.aubree@epitech.eu>
2024-03-09 11:51:16 +0100

    Push fonction récuperation image

--- script.js
+++ script.js
@@ -5,6 +5,20 @@
 ** script.js
 */
 
+async function fetchItemImage(itemId) {
+    try {
+        const response = await fetch(`https://api.kedufront.juniortaker.com/item/picture/${itemId}`);
+        if (!response.ok) {
+            throw new Error('La récupération de l\'image a échoué.');
+        }
+        const blob = await response.blob();
+        return URL.createObjectURL(blob);
+    } catch (error) {
+        console.error('Une erreur s\'est produite lors de la récupération de l\'image:', error);
+        return null;
+    }
+}
+
 async function fetchAndDisplayItems() {
     try {
         const response = await fetch('https://api.kedufront.juniortaker.com/item/');
@@ -15,21 +29,23 @@ async function fetchAndDisplayItems() {
             return;
         }
 
-        items.forEach(item => {
+        for (const item of items) {
+            const imageSrc = await fetchItemImage(item._id);
             const itemContainer = document.createElement('div');
             itemContainer.classList.add('item');
             const itemHTML = `
-                <img src="${item.image}" alt="${item.name}">
+                <img src="${imageSrc}" alt="${item.name}">
                 <h3>${item.name}</h3>
                 <p>Description: ${item.description}</p>
                 <p>Prix: ${item.price}€</p>
                 <p>Date de création: ${item.createdIn}</p>
             `;
             itemContainer.innerHTML = itemHTML;
-
             container.appendChild(itemContainer);
-        });
+        }
     } catch (error) {
         console.error('Une erreur s\'est produite lors de la récupération des items :', error);
     }
 }
+
+document.addEventListener('DOMContentLoaded', fetchAndDisplayItems);
